069a80584f55fe7a10daefe052d10c72e5284660
AlejoBengo <alejobengoechea@yahoo.com.ar>
2022-04-20 22:48:33 -0300

    version 1.0.8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,29 @@
 //_______________________________________________________________________
 /*IMPORT DE UTILS*/
 import React from "react";
+import { useSelector } from "react-redux";
 /*IMPORT DE COMPONENTES*/
 import SearchBar from "./SearchBar";
-import Cards from "./Cards";
+import Card from "./Card";
 /*IMPORT DE CSS*/
 import s from "../estilos/home.module.css";
 //_______________________________________________________________________
 
 export default function Home() {
+  const { city } = useSelector((state) => {
+    return state;
+  });
   return (
-    <div className={s.cardsContainer}>
+    <div className={s.container}>
       <SearchBar />
-      <div>
-        <Cards />
+
+      <div className={s.cardsContainer}>
+        {city.length > 0 &&
+          city.map((data) => (
+            <div key={data.id}>
+              <Card data={data} />
+            </div>
+          ))}
       </div>
     </div>
   );
